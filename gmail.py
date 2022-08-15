c5e13d00b5d4a57255e1cf42a265857e784f3394
Robson Caldas <p25@rcaldas.com>
2022-08-15 20:54:45 -0300

    Identifie and extract data from PDF

--- gmail.py
+++ gmail.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 from os import getenv, path
+import locale
 import base64
 import email
+import pytz
 
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
@@ -18,6 +20,8 @@ db = MongoClient(MONGO_URI).get_database()
 fs = gridfs.GridFS(db)
 maildb = db.mail
 
+tz = pytz.timezone("America/Sao_Paulo")
+
 # Path to file with Google Credentials
 AUTH_FILE = 'oauth.json'
 # Scope of API to generate the token file in 'token.json' path
@@ -28,6 +32,42 @@ LABEL = 'Boragora'
 # From email header content to match
 FROM = 'magmacontabilidade'
 
+# List of Doc types expected to get from attachment files
+DOC_TYPES = [
+    {
+        # String to identify this type of Document
+        'string': 'Documento de Arrecadação do Simples Nacional',
+        'name': 'Imposto Simples Nacional',
+        # Mongodb collection to save the new object
+        'collection': 'expense',
+        # List of info with positions to get from file data
+        'get': [
+            {
+                'name': 'CNPJ',
+                # Value *must* be equal to validate the file
+                'value': '42.457.552/0001-73',
+                # Positions in file's data where info appears
+                'positions': [2,71],
+            },
+            {
+                'name': 'Valor',
+                'positions': [12,55,56,77],
+                # Type of data to convert, default=string (as data ir read from file)
+                'type': 'float',
+            },
+            {
+                'name': 'Periodo',
+                'positions': [15],
+                'type': 'date',
+            },
+            {
+                'name': 'Vencimento',
+                'positions': [9,16,75],
+                'type': 'date',
+            },
+        ]
+    },
+]
 
 def main():
     # Get GMail service
@@ -67,6 +107,7 @@ def main():
                 mail_item['subject'] = k['value']
             elif k['name'] == 'Date':
                 date = k['value'].split('(')[0].strip()
+                # locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
                 mail_item['date'] = datetime.strptime(date, '%a, %d %b %Y %X %z')
 
         # Check sender
@@ -148,23 +189,106 @@ def get_file():
             # with open(f'/tmp/{filename}', 'wb') as f:
             #     f.write(fs.get(i['files'][0]['id']).read())
 
-            import_pdf(fs.get(i['files'][0]['id']))
-            return
+            return import_pdf(fs.get(i['files'][0]['id']))
     print('No files')
 
 def import_pdf(file):
     if file.content_type != 'application/pdf':
         print('Not pdf')
         return False
+
+    # Extract all data from PDF
     data = []
     with fitz.open(stream=file.read(), filetype='pdf') as doc:
         pages = doc.page_count
         for page in doc:
             data += page.get_text().split('\n')
 
-    for i in data:
-        print(i)
-    print(f'\nPages: {pages}')
+    # Identify data positions (index of data list), change for each document, kept for history
+    for i, x in enumerate(data):
+        # print(x)
+
+        # # Find and check CNPJ
+        # if x == '42.457.552/0001-73':
+        #     print(i)
+        # # Value in positions 2,71 | put in DOC_TYPES 
+
+        # # Find Valor position
+        # if x == '13.567,44':
+        #     print(i)
+        # # Value in positions 12,55,56,77 | put in DOC_TYPES 
+
+        # # Find Periodo
+        # if x == 'Julho/2022':
+        #     print(i)
+        # # Value in position 15 | put in DOC_TYPES 
+
+        # # Find Vencimento position
+        # if x == '22/08/2022':
+        #     print(i)
+        # # Value in positions 9,16,75 | put in DOC_TYPES 
+
+        pass
+
+    doc = {}
+    # Identify file type
+    for dtype in DOC_TYPES:
+        # First document type have the string splited in the two first data items
+        if dtype['string'] == ' '.join(data[:2]):
+            doc['name'] = dtype['name']
+            # print(dtype['name'])
+
+            # Get values
+            for i in dtype['get']:
+                # Get value from first position
+                value = data[i['positions'][0]]
+                # If more than 1 position, check if all positions match
+                if len(i['positions']) > 1:
+                    for p in i['positions'][1:]:
+                        if value != data[p]:
+                            print(f'"{i["name"]}" position values do not match:\n{value} | {data[p]}')
+                            return False
+
+                # Check if value match
+                if i.get('value'):
+                    if value != i['value']:
+                        print(f"Wrong {i['name']}:\n{value} | {i['value']}")
+                        return False
+
+                # Format value type
+                if i.get('type'):
+                    # match i['type']:
+                    if i['type'] == 'float':
+                        value = value.replace('.', '').replace(',', '.')
+                    elif i['type'] == 'date':
+                        try:
+                            value = datetime.strptime(value, '%d/%m/%Y').date()
+                            # Treat error in mongodb to encode datetime.date object
+                            value = str(value)
+                        except ValueError:
+                            # Treat locale language
+                            locale.setlocale(locale.LC_ALL, 'pt_BR.UTF-8')
+                            value = datetime.strptime(value, '%B/%Y').date()
+                            value = str(value)
+                    else:
+                        print(f"Invalid data type: {i['type']}")
+                        return False
+
+                doc[i['name']] = value
+                # print(f"{i['name']}: {value}")
+            
+            print(doc)
+
+            coll = eval(f"db.{dtype['collection']}")
+            result = coll.insert_one(doc)
+            
+            print(result.inserted_id)
+
+            return(result.inserted_id)
+            
+
+    # print(f'\nPages: {pages}')
+    return data
 
 if __name__ == '__main__':
     # ! Clear database
